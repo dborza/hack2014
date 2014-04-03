24cca2e0f203288c218e4609f88ead4f78feea35
Dan Borza <dan.borza@intel.com>
2014-04-03 16:17:34 +0300

    Added comment to deploy script

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+#
+#   Script for downloading, compiling and deploying the app. Simple and efficient.
+#
 git pull
 mvn clean install package
 java -jar target/gs-accessing-data-rest-0.1.0.jar
